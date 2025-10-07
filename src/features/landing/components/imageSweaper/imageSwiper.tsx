c7caeb1976e295b60f5b20734c2ced54c0b16df3
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-07 11:50:48 +0330

    swiper added

---
+++ src/features/landing/components/imageSweaper/imageSwiper.tsx
@@ -0,0 +1,51 @@
+import React, { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+interface ImageSwiperProps {
+  images: string[];
+}
+
+const ImageSwiper: React.FC<ImageSwiperProps> = ({ images }) => {
+  const progressCircle = useRef<SVGSVGElement | null>(null);
+  const progressContent = useRef<HTMLSpanElement | null>(null);
+
+  const onAutoplayTimeLeft = (_: any, time: number, progress: number) => {
+    if (progressCircle.current && progressContent.current) {
+      progressCircle.current.style.setProperty('--progress', `${1 - progress}`);
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      pagination={{ clickable: true }}
+      navigation
+      modules={[Autoplay, Pagination, Navigation]}
+      onAutoplayTimeLeft={onAutoplayTimeLeft}
+      className="mySwiper"
+    >
+      {images.map((img, index) => (
+        <SwiperSlide key={index}>
+          <img src={img} alt={`Slide ${index + 1}`} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImageSwiper;
--- src/features/landing/index.tsx
+++ src/features/landing/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import ImageSwiper from './components/imageSweaper/imageSwiper';
+import styles from './styles.module.css';
+
+const images = [
+  'https://janebi.com/janebi/9fd2/files/normal/496502.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496501.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496499.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496497.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496495.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496498.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496494.jpg',
+];
 
 const Landing: React.FC = () => (
-  <div>
-    <h2>Dashboard</h2>
-    <p>Protected content for logged-in users.</p>
+  <div className={styles.container}>
+    <ImageSwiper images={images} />
   </div>
 );
 
