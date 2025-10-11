7dab17478c1bc0af6c2b5dec0fb2cfe8ab0a77d9
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-11 12:41:02 +0330

    filtering got modified and is usable

--- src/features/products/filtering/Filtering.tsx
+++ src/features/products/filtering/Filtering.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import styles from './styles.module.css';
 import Icons from '../../../shared/icons';
+import { useNavigate, createSearchParams } from 'react-router';
+
+const categories = [
+  { label: 'همه', value: '' },
+  { label: 'الکترونیکی', value: 'electronics' },
+  { label: 'جواهرات', value: 'jewelery' },
+  { label: 'لباس مردانه', value: "men's clothing" },
+  { label: 'لباس زنانه', value: "women's clothing" },
+];
 
 const Filtering: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (value: string) => {
+    const params = value ? { filter: value } : {};
+    navigate({
+      pathname: '/products',
+      search: `?${createSearchParams(params)}`,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <h2>
-        <Icons name="bx-filter"/> دسته بندی ها
+        <Icons name="bx-filter" /> دسته بندی ها
       </h2>
       <ul>
-        <li>همه</li>
-        <li>الکترونیکی</li>
-        <li>جواهرات</li>
-        <li>لباس مردانه</li>
-        <li>لباس زنانه</li>
+        {categories.map((cat) => (
+          <li key={cat.value} onClick={() => handleClick(cat.value)}>
+            {cat.label}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/features/products/index.tsx
+++ src/features/products/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import Filtering from './filtering/Filtering';
 import Cards from './cards/Cards';
 import { useQuery } from '@tanstack/react-query';
 import { fetchProducts } from '../landing/services/services';
+import { useSearchParams } from 'react-router';
 
 const Products: React.FC = () => {
-  const { data: products } = useQuery({
+  const {
+    data: products,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ['products'],
     queryFn: fetchProducts,
   });
+  console.log(products);
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get('filter') || '';
+
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    if (!products) return;
+
+    if (!filter) {
+      setFilteredProducts(products);
+    } else {
+      setFilteredProducts(
+        products.filter((p) =>
+          p.category.toLowerCase().includes(filter.toLowerCase())
+        )
+      );
+    }
+  }, [products, filter]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong.</p>;
+
   return (
     <div className={styles.container}>
       <Filtering />
-      <Cards data={products} />
+      <Cards data={filteredProducts} />
     </div>
   );
 };
