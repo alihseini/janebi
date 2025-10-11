df600a66282263b39819e16cfb9188885bab5854
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-11 10:46:43 +0330

    styles added tp product page

--- src/features/products/filtering/Filtering.tsx
+++ src/features/products/filtering/Filtering.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './styles.module.css';
+import Icons from '../../../shared/icons';
 
 const Filtering: React.FC = () => {
   return (
     <div className={styles.container}>
+      <h2>
+        <Icons name="bx-filter"/> دسته بندی ها
+      </h2>
       <ul>
         <li>همه</li>
         <li>الکترونیکی</li>
