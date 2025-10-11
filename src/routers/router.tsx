d508a043390bd1ee76faa12ba090ed8d703dbf2c
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-11 10:55:31 +0330

    router version updated

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,24 +1,21 @@
 import { lazy } from 'react';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Navigate,
-} from 'react-router';
+import { createBrowserRouter, Navigate } from 'react-router';
 import Layout from '../layout';
 
 const Landing = lazy(() => import('../features/landing'));
 const Products = lazy(() => import('../features/products'));
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path="landing" element={<Landing />} />
-        <Route path="products" element={<Products />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </>
-  )
-);
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: 'landing', element: <Landing /> },
+      { path: 'products', element: <Products /> },
+    ],
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
+  },
+]);
