295e2cf0c0e4366b18ea6b8875dea8a5b2441b51
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-15 10:08:35 +0330

    scroll staying at its place when changing page bug fixed

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 import Header from './components/header/Header';
 import styles from './style.module.css';
 import Footer from './components/footer/Footer';
 
 const MainLayout: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
   return (
     <>
       <header className={styles.fixedHeader}>
