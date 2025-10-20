d07baca659e3235af2b3f26412e96f32631cf343
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-20 12:27:32 +0330

    feat: add button to scroll in y direction

--- src/shared/components/cardList/CardList.tsx
+++ src/shared/components/cardList/CardList.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import CardMaker from '../cardMaker/CardMaker';
 import styles from './styles.module.css';
 import Button from '../button/Button';
 import { useNavigate } from 'react-router';
 
 interface CardListProps {
-  data: [];
+  data: any[];
   title: string;
   button: string;
 }
 
 const CardList: React.FC<CardListProps> = ({ data, title, button }) => {
   const navigate = useNavigate();
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  const scroll = (direction: 'left' | 'right') => {
+    if (scrollRef.current) {
+      const scrollAmount = 300;
+      scrollRef.current.scrollBy({
+        left: direction === 'right' ? scrollAmount : -scrollAmount,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <div className={styles.cardList}>
       <div className={styles.upperCardList}>
@@ -23,14 +35,31 @@ const CardList: React.FC<CardListProps> = ({ data, title, button }) => {
           onClick={() => navigate('/products')}
         />
       </div>
-      <div className={styles.allCards}>
-        {data?.map((item) => (
-          <CardMaker
-            key={item.id}
-            product={item}
-            onClick={() => console.log('click')}
-          />
-        ))}
+
+      <div className={styles.scrollContainer}>
+        <Button
+          color="#0089ff"
+          text="<"
+          className={styles.rightButton}
+          onClick={() => scroll('right')}
+        />
+
+        <div className={styles.allCards} ref={scrollRef}>
+          {data?.map((item) => (
+            <CardMaker
+              key={item.id}
+              product={item}
+              onClick={() => console.log('click')}
+            />
+          ))}
+        </div>
+
+        <Button
+          text=">"
+          color="#0089ff"
+          className={styles.leftButton}
+          onClick={() => scroll('left')}
+        />
       </div>
     </div>
   );
