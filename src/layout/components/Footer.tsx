e67542268e2d79f874afe59f23e55a19f291d757
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-21 17:16:35 +0330

    feat: complete footer

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from '../css/styles.module.css';
+import Button from '../../shared/components/Button/Button';
+import Icons from '../../shared/icons';
 
 const Footer: React.FC = () => {
   return (
@@ -36,6 +38,91 @@ const Footer: React.FC = () => {
           />
           <p>پرداخت امن</p>
           <span>درگاه پرداخت مطمئن</span>
+        </div>
+      </div>
+      <div className={styles.mainFooter}>
+        <div>
+          <p>راهنمای خرید از جانبی</p>
+          <ul>
+            <li>ثبت سفارش</li>
+            <li>رویه ارسال سفارشات</li>
+            <li>پیگیری سفارش</li>
+            <li>راهنمای پرداخت</li>
+          </ul>
+        </div>
+        <div>
+          <p>خدمات مشتریان</p>
+          <ul>
+            <li>پاسخ به سوالات متداول</li>
+            <li>رویه بازگرداندن کالا</li>
+            <li>شرایط استفاده</li>
+            <li>حریم خصوصی</li>
+          </ul>
+        </div>
+        <div>
+          <p>جانبی</p>
+          <ul>
+            <li>درباره جانبی</li>
+            <li>تماس با جانبی</li>
+            <li>همکاری با جانبی</li>
+            <li>شماره حساب های جانبی</li>
+            <li>وبلاگ جانبی</li>
+          </ul>
+        </div>
+        <div>
+          <p>دریافت اپلیکیشن</p>
+          <div className={styles.download}>
+            <Button
+              text="دانلود اپلیکیشن اندروید"
+              svgSrc="Vector"
+              className={styles.downloadButton}
+            />
+            <img
+              src="https://janebi.com/janebi/9fd2/uploads/theme/banner/footer-appv2.webp"
+              alt="footer"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.bottomFooter}>
+        <p>
+          پاسخگوی شما هستیم : شنبه تا چهارشنبه<span> 9-17 </span>. پنجشنبه{' '}
+          <span>9-14</span>
+        </p>
+        <div className={styles.bottomFooterBase}>
+          <div className={styles.bottomInfo}>
+            <div>
+              <Icons name="bx-phone" color="#0089ff" /> 021-629999777
+            </div>
+            <div>
+              <Icons name="bx-envelope" color="#0089ff" /> info@janebi.com
+            </div>
+          </div>
+          <div>
+            <Button svgSrc="telegram" />
+            <Button svgSrc="insta" />
+            <Button svgSrc="facebook" />
+            <Button svgSrc="tweeter" />
+            <Button svgSrc="linkedin" />
+          </div>
+          <div>
+            <img
+              src="https://janebi.com/janebi/9fd2/uploads/theme/banner/econ.svg"
+              alt="econ"
+            />
+            <img
+              src="https://janebi.com/janebi/9fd2/uploads/theme/banner/enamad.png"
+              alt="etemad"
+            />
+            <img
+              src="https://janebi.com/janebi/9fd2/uploads/theme/banner/anjoman.webp"
+              alt="anjoman"
+            />
+            <img
+              src="https://janebi.com/janebi/9fd2/uploads/theme/banner/samandehi.webp"
+              alt="samandehi"
+            />
+          </div>
         </div>
       </div>
     </div>
