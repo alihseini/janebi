1a841f0d66605957353b7135acaabdf9a910ff4c
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-22 17:26:37 +0330

    fix: change useEffecr dependencies

--- src/features/products/page/ProductDetails.tsx
+++ src/features/products/page/ProductDetails.tsx
@@ -1,24 +1,28 @@
 import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
 import CardList from '../../../shared/components/CardList/CardList';
-import MainDetails from '../components/MainDetalis';
+import MainDetails from '../components/MainDetails';
 import styles from '../css/styles.module.css';
 import { useProductById, useProducts } from '../services/useProducts';
-import { useEffect, useState } from 'react';
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
   const { data, isLoading, isError } = useProductById(id || '');
   const { data: products = [] } = useProducts();
 
-  const [filteredCards, setFilteredCards] = useState([]);
+  const [filteredCards, setFilteredCards] = useState<any[]>([]);
 
   useEffect(() => {
-    let temp = products.filter((item) => item?.category === data?.category);
+    if (!data || !products.length) return; 
+    const temp = products.filter(
+      (item) => item?.category === data?.category && item?.id !== data?.id
+    );
     setFilteredCards(temp);
-  }, []);
+  }, [data, products]);
 
   if (isLoading) return <p>Loading...</p>;
   if (isError || !data) return <p>Something went wrong.</p>;
+
   return (
     <div className={styles.detailsPage}>
       <MainDetails data={data} />
