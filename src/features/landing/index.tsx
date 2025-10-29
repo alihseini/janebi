c274547c15b1c1ad0edb35932865321d68f3872b
Ali Hosseini <a.hosseini9@outlook.com>
2025-10-29 12:56:17 +0330

    chore: add json folder for static datas

--- src/features/landing/index.tsx
+++ src/features/landing/index.tsx
@@ -6,21 +6,7 @@ import BannerMaker from '../../shared/components/BannerMaker/BannerMaker';
 import TripleBannerCards from './components/TripleBannerCards';
 import FourBanner from './components/FourBanners';
 import { useProducts } from '../products/services/useProducts';
-
-const images = [
-  'https://janebi.com/janebi/9fd2/files/normal/496502.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496501.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496499.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496497.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496495.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496498.jpg',
-  'https://janebi.com/janebi/9fd2/files/normal/496494.jpg',
-];
-
-const imageBanner = [
-  'https://janebi.com/janebi/9fd2/uploads/birthday404/fix-banner_powerbank.jpg',
-  'https://janebi.com/janebi/9fd2/uploads/birthday404/fix-banner_headphone.jpg',
-];
+import { imageBanner, images } from './json/images';
 
 const Landing: React.FC = () => {
   const { data: products, isLoading, isError, error } = useProducts();
---
+++ src/features/landing/json/images.ts
@@ -0,0 +1,14 @@
+export const images = [
+  'https://janebi.com/janebi/9fd2/files/normal/496502.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496501.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496499.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496497.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496495.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496498.jpg',
+  'https://janebi.com/janebi/9fd2/files/normal/496494.jpg',
+];
+
+export const imageBanner = [
+  'https://janebi.com/janebi/9fd2/uploads/birthday404/fix-banner_powerbank.jpg',
+  'https://janebi.com/janebi/9fd2/uploads/birthday404/fix-banner_headphone.jpg',
+];
